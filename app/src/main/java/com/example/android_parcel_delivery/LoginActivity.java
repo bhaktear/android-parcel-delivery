a9cf2394266e5b85c3473c6abec222c4f7777013
bhaktear <bhaktear@gmail.com>
2023-08-19 10:17:48 +0600

    login

--- app/src/main/java/com/example/android_parcel_delivery/LoginActivity.java
+++ app/src/main/java/com/example/android_parcel_delivery/LoginActivity.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.android_parcel_delivery.libs.OkHttpUtils;
+import com.example.android_parcel_delivery.libs.ServerRequest;
+import com.example.android_parcel_delivery.libs.Utils;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,42 +29,62 @@ public class LoginActivity extends AppCompatActivity  {
         userId = findViewById(R.id.userID);
         loginPassword = findViewById(R.id.password);
         loginBtn = findViewById(R.id.submit);
+        //Utils.alert(this,"Hello");
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String user_id = userId.getText().toString();
-                String password = loginPassword.getText().toString();
-                if(!user_id.equals("") && !password.equals("")){
-                    Map<String, String> postData = new HashMap<>();
-                    postData.put("user_id",user_id);
-                    postData.put("password",password);
-                    String url = ServerRequest.makeURL("api/login.php");
-                    //Toast.makeText(LoginActivity.this,url,Toast.LENGTH_SHORT).show();
-
-                    ServerRequest.performRequest("POST", url, postData, new ServerRequest.ServerResponseListener() {
-                        @Override
-                        public void onResponse(int err, String msg) {
-                            if(err == 0){
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-                            }else{
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-                            }
-                        }
-                    });
-                }
+                login();
             }
         });
 
+    }
+
+    private void login() {
+        String user_id = userId.getText().toString();
+        String password = loginPassword.getText().toString();
+        if(!user_id.equals("") && !password.equals("")) {
+            Map<String, String> postData = new HashMap<>();
+            postData.put("user_id", user_id);
+            postData.put("password", password);
+            String url = Utils.makeURL("api/login.php");
+
+            OkHttpUtils okHttpUtils = new OkHttpUtils();
+            okHttpUtils.postRequest(url, postData, new OkHttpUtils.ResponseCallback() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.d("TAG","reg" + response);
+                    try{
+                        String msg = response.getString("msg");
+                        int err = response.getInt("err");
+                        //Log.d("TAG","reg- " + msg);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                //Utils.alert(LoginActivity.this,msg,"Ok","");
+                                if(err == 0){
+                                    //call dashboard
+                                    Intent intent = new Intent(LoginActivity.this,NavigationActivity.class);
+                                    startActivity(intent);
+                                }else{
+                                    Utils.alert(LoginActivity.this,msg,"Ok","");
+                                }
+                            }
+                        });
+
+
+
+                    }catch (Exception e){
+                        //Log.d("TAG","exp" + e.getMessage());
+                    }
+                }
+
+                @Override
+                public void onError(String error) {
+
+                }
+            });
+
+        }
     }
 
     public void registerActivity(View view){
--- app/src/main/java/com/example/android_parcel_delivery/ServerOkHttp.java
+++
@@ -1,9 +0,0 @@
-package com.example.android_parcel_delivery;
-
-
-
-public class ServerOkHttp {
-    //private OkHttpClient client;
-
-
-}
